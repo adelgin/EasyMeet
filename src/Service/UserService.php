d39ba326a9d93abad8e0f62e16f22bb220fb902a
adelgin <leto.456@mail.ru>
2025-07-04 16:58:51 +0300

    nedocomitil

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -2,7 +2,13 @@
 
 namespace App\Service;
 
+use App\Repository\UserRepository;
+
 class UserService
 {
+    public function __construct(public UserRepository $userRepository) {}
 
+    public function getAll(): array {
+        return $this->userRepository->findAll();
+    }
 }
